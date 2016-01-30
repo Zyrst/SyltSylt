cffccf80eae42fae793a89c36a87f25860d737d0
Zyrst <zajreg@gmail.com>
2016-01-30 14:29:01 +0100

    hopp

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour {
     private Rigidbody2D mBody;
     public float Speed;
     private bool mJumped = false;
+    private float mDistToGround;
 	// Use this for initialization
 	void Start () {
         
@@ -16,26 +17,18 @@ public class PlayerMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         float sumX = 0;
-        float sumY = 0;
-        mJumped = false;
-        if (Input.GetButton("Horizontal" + joyNum) && Input.GetAxisRaw("Horizontal" + joyNum) > 0)
+        //mJumped = false;
+        if (Input.GetButton("Horizontal" + joyNum))
         {
-            sumX += 1;
+            sumX += Input.GetAxisRaw("Horizontal" + joyNum);
         }
-        if (Input.GetButton("Horizontal" + joyNum) && Input.GetAxisRaw("Horizontal" + joyNum) < 0)
-        {
-            sumX -= 1;
-        }
-        if(Input.GetButtonDown("Jump" + joyNum) && !mJumped)
-        {
-            mJumped = true;
-        }
-        sumX *= (Speed * Time.deltaTime);
-        transform.position += new Vector3(sumX, sumY);
-        if(mJumped)
+        if(Input.GetButtonDown("Jump" + joyNum) && OnGround())
         {
             mBody.AddForce(new Vector2(0, 10), ForceMode2D.Impulse);
         }
+        sumX *= (Speed * Time.deltaTime);
+        transform.position += new Vector3(sumX, 0);
+        
         
 	}
 
@@ -43,5 +36,24 @@ public class PlayerMovement : MonoBehaviour {
     {
         joyNum = playerNum;
         mBody = body;
+        mDistToGround = GetComponent<Collider2D>().bounds.extents.y;
+    }
+
+    public bool OnGround()
+    {
+        Debug.DrawRay(transform.position - new Vector3(0, GetComponent<Collider2D>().bounds.extents.y), -Vector3.up - new Vector3(0, 0.1f, 0), Color.red, 10f);
+        if (Physics2D.Raycast(transform.position - new Vector3(0, GetComponent<Collider2D>().bounds.extents.y), -Vector2.up, mDistToGround))
+        {
+            try
+            {
+                return true;
+            }
+            catch (System.NullReferenceException) { return false; }
+        }
+        else
+        {
+            return false;
+        }
+        
     }
 }
