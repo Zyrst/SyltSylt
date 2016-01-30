e43936b4bf44c0a635315944d526caeba88e97ef
unknown <daniel.dalebjork@hotmail.com>
2016-01-30 14:22:58 +0100

    Teleport

---
+++ Assets/Scripts/TeleporterScript.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TeleporterScript : MonoBehaviour {
+
+    struct Pair {
+        public GameObject player;
+        public float timer;
+
+        public Pair(GameObject go, float in_timer)
+        {
+            player = go;
+            timer = in_timer;
+        }
+
+        public void CountDownTimer(float dt)
+        {
+            timer -= dt;
+        }
+
+        public float GetTimer()
+        {
+            return timer;
+        }
+        public GameObject GetObject()
+        {
+            return player;
+        }
+    }
+
+    public GameObject mExit;
+    public float mMaxTimer;
+
+    List<Pair> mPair = new List<Pair>();
+
+	// Use this for initialization
+	void Start () {
+
+        mMaxTimer = 0.5f;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    
+        for (int i = 0; i < mPair.Count; i++)
+        {
+            mPair[i].CountDownTimer(Time.deltaTime);
+            if(mPair[i].GetTimer() <= 0.0f)
+            {
+                mPair[i].GetObject().transform.position = mExit.transform.position;
+                mPair[i].GetObject().SetActive(true);
+            }
+            mPair.RemoveAt(i);
+            i--;
+        }
+	}
+
+     void OnCollisionEnter2D (Collision2D collisionObject) {
+        if (collisionObject.gameObject.tag == "Player")
+        {
+            mPair.Add(new Pair(collisionObject.gameObject, mMaxTimer));
+
+            collisionObject.gameObject.SetActive(false);
+            Vector2 tempPos = new Vector2(-100000.0f, -10000.0f);
+            collisionObject.gameObject.transform.position = tempPos;
+
+            /*if(collisionObject.GETSTRUCT DOWN == true)
+            {
+                collisionObject.setActive = false;
+                collisionObject.transform.position = mPair.transform.position;
+                collisionObject.GETSTRUCT DOWN = false;
+            }*/
+        }
+     }
+}
