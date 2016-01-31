22028b03dd37da4987cc7f582e83febe87a1d933
jeflux <jespermathiason@gmail.com>
2016-01-31 12:29:30 +0100

    Audio

---
+++ Assets/Audio/AudioSourcePrefabScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioSourcePrefabScript : MonoBehaviour {
+
+	AudioSource source;
+
+	void Start () {	
+		source = GetComponent<AudioSource>();
+	}
+	
+	void Update () {
+		if (!source.isPlaying)
+			GameObject.Destroy(transform.gameObject);
+	}
+}
